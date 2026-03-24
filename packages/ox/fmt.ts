1edc5a6cd8c7dd7c27a031dfefea5d455a9c6daf
Bryan Djafer <yukky.bryan@gmail.com>
2026-03-24 12:21:04 +0100

    chore: format with oxfmt

--- packages/ox/fmt.ts
+++ packages/ox/fmt.ts
@@ -1,3 +1,25 @@
 import { defineConfig } from 'oxfmt'
 
-export default defineConfig({})
+export default defineConfig({
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  ignorePatterns: ['*.min.js', '*.min.css', '*.tsbuildinfo', '**/*.md'],
+  sortImports: {
+    groups: [
+      'type-import',
+      ['value-builtin', 'value-external'],
+      'type-internal',
+      'value-internal',
+      ['type-parent', 'type-sibling', 'type-index'],
+      ['value-parent', 'value-sibling', 'value-index'],
+      'unknown',
+    ],
+  },
+})
